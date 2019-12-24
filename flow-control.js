576be68a941046a4333dd8ee2eb02ab2350ae9f5
Taulant Ibraj <tibraj94@gmail.com>
2019-12-24 17:21:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,8 +12,14 @@ function teenager(age) {
     }
   }
 function ageChecker(age) {
-
-}
+  if(age>=13&&age<=19){
+    return("You are a teenager!");
+  }else if(age<=13){
+    return("You are a kid");
+  }else if(age>=19){
+    return('You are a grownup');
+  }
+  }
 
 function ternaryTeenager(age) {
 
